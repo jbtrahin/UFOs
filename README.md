69e2cc5027c3a7afb52bbd1c7b99fb2e60fd7053
JB Trahin <jb.trahin@gmail.com>
2019-12-22 13:46:35 -0800

    Updating files after module 11 challenge

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # UFOs
 Using JavaScript, HTML, and CSS to create a custom webpage that showcases different UFO sightings around the world.
+
+## Module Overview
+In this module, you will create a table to organize UFO data that is stored as a JavaScript array, or list. This table will have the ability to filter data based on certain criteria and will be created using JavaScript as the primary coding language. To be successful in this module, you’ll need to apply your HTML and Bootstrap skills and have an open mind regarding semicolons.
+
+## Project Overview
+In this challenge, you will create additional filters for the webpage. The advanced filtering capability will allow users to filter the data by multiple factors.
+
+The goals for this challenge are to:
+1. Create, update, and deploy JavaScript functions to provide additional table filters.
+2. Update and deploy forEach (for loop) to loop through the filters and update them with user input.
+3. Update and populate the dynamic filters and table using JavaScript and HTML.
+
+## Resources
+
+- Software: Visual Studio Code 1.39.0
+- Languages: JavaScript, HTML, CSS, Bootstrap
+
+## App Preview
+
+Here is a preview of what the web app looks like once all the new filters have been created and are functional:
+![alt text](https://github.com/jbtrahin/Module7-Challenge-JB-Trahin/blob/master/web_app_example.png)
+
+## Recommendations for further development
+
+Here are some suggestions on what I'd like to tackle next:
+- Have the sightings data populate table tableData in a dynamic manner (through another website's API for example).
+- Expand the search filter options to the other columns available in the table.
+- Turn out text filter into drop-down lists to constrain and guide users to the right inputs
+- We could enable the "enter" keyboard key as way to trigger the search button instead of having to press the button with the mouse.
+- Create a search functionality for the comments column and pair it to a search bar.
--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,16 @@
-// import the data from data.js
+// from data.js
 const tableData = data;
 
-// Reference the HTML table using d3
+// get table references
 var tbody = d3.select("tbody");
 
-// Build a function to pass in data from data.js
+// This function will build table
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
-    // Next, loop through each object in the data and append a row and cells for each value in the row
+
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
         // Append a row to the table body
         let row = tbody.append("tr");
@@ -20,22 +22,79 @@ function buildTable(data) {
     });
 }
 
-// Build a function to filter table
-function handleClick() {
-    // Grab the datetime value from the filter
+// Filters: Data, City, State, Country, Shape
+// Keep track of all filters
+var filters = {};
+
+// This function will update the filters based on input
+function updateFilters() {
+
+    // Save the element, value, and id of the filter that was changed
     let date = d3.select("#datetime").property("value");
-    let filteredData = tableData;
-    // Check to see if a date was entered and filter the data using that date.
+    let city = d3.select("#cityfilter").property("value");
+    let state = d3.select("#statefilter").property("value");
+    let country = d3.select("#countryfilter").property("value");
+    let shape = d3.select("#shapefilter").property("value"); 
+
+    // If a filter value was entered then add that filterId and value to the filters list. Otherwise, clear that filter from the filters object.
+    
     if (date) {
-      // Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value
-      filteredData = filteredData.filter(row => row.datetime === date);
+        filters.datetime = date;    
+    } else {
+        filters.datetime = null;
     }
-    // Rebuild the table using the filtered data
-    // @NOTE: If no date was entered, then filteredData will just be the original tableData.
-    buildTable(filteredData);
+    
+    if (city) {
+        filters.city = city;
+    } else {
+        filters.city = null;
+    }
+    
+    if (state) {
+        filters.state = state;
+    } else {
+        filters.state = null;
+    }
+    
+    if (country) {
+        filters.country = country;
+    } else {
+        filters.country = null;
+    }
+    
+    if (shape) {
+        filters.shape = shape;
+    } else {
+       filters.shape = null;
+    }
+
+    // Call function to apply all filters and rebuild the table
+    filterTable();
 }
 
-// Attach an event to listen for the form button
-d3.selectAll("#filter-btn").on("click", handleClick);
+// This function will filter the table
+function filterTable() {
+
+    // Set the filteredData to the tableData 
+    let filteredData = tableData; 
+
+    // Loop through all of the filters and keep any data that matches the filter values
+    // Using a for...of loop instead of forEach as I found it was better practice.
+    // forEach loop would start like this: 
+    // Object.entries(filters).forEach(([key, value])
+    
+    for(const [key, value] of Object.entries(filters)) {  
+        if (value) {
+            filteredData = filteredData.filter(row => row[key] === value);
+        }
+    }
+
+  // Finally, rebuild the table using the filtered Data
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for changes to each filter
+d3.selectAll("#filter-btn").on("click", updateFilters);
+
 // Build the table when the page loads
 buildTable(tableData);
